16bc02e43274c0364f58562aa3421f8fc409165d
Shoaib Khan <devsbuddy404@gmail.com>
2022-01-14 23:30:42 +0530

    Fixed config loading where it was throwing error while installing app

--- app/Providers/MailConfigServiceProvider.php
+++ app/Providers/MailConfigServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class MailConfigServiceProvider extends ServiceProvider
@@ -26,16 +28,18 @@ class MailConfigServiceProvider extends ServiceProvider
     public
     function boot()
     {
-        $config = array(
-            'driver' => getSetting('mail_driver'),
-            'host' => getSetting('mail_host'),
-            'port' => getSetting('mail_port'),
-            'from' => array('address' => getSetting('mail_from_email'), 'name' => getSetting('mail_from_name')),
-            'encryption' => getSetting('mail_encryption'),
-            'username' => getSetting('mail_username'),
-            'password' => getSetting('mail_password'),
-        );
-        $mailConfig = array_merge(config('mail'), $config);
-        Config::set('mail', $mailConfig);
+        if (DB::connection()->getDatabaseName()) {
+            $config = array(
+                'driver' => getSetting('mail_driver'),
+                'host' => getSetting('mail_host'),
+                'port' => getSetting('mail_port'),
+                'from' => array('address' => getSetting('mail_from_email'), 'name' => getSetting('mail_from_name')),
+                'encryption' => getSetting('mail_encryption'),
+                'username' => getSetting('mail_username'),
+                'password' => getSetting('mail_password'),
+            );
+            $mailConfig = array_merge(config('mail'), $config);
+            Config::set('mail', $mailConfig);
+        }
     }
 }
