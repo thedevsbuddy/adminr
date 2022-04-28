906ab8379b86613667934d57120e7f280c3e0fdb
Shoaib Khan <devsbuddy404@gmail.com>
2022-04-29 04:15:43 +0530

    Added x-link blade component.

---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,19 @@
+@props(['as' => 'link', 'method' => 'get', 'href' => '#'])
+
+
+@if($as == 'link')
+    <a {{ $attributes->merge(['href' => $href]) }}>
+        {{ $slot }}
+    </a>
+@endif
+
+@if($as == 'form')
+    <form action="{{ $href }}" method="{{ $method }}">
+        @if(strtoupper($method) == 'POST' || strtoupper($method) == 'PUT')
+            @csrf
+        @endif
+    </form>
+    <a {{ $attributes->merge(['href' => $href]) }}>
+        {{ $slot }}
+    </a>
+@endif
