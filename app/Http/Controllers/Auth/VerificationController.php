a6c25a4c3342e8a8e5b5f7a0c178e9d57f5765f6
Shoaib Khan <devsbuddy404@gmail.com>
2022-05-01 04:28:49 +0530

    Added verification methods

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -3,19 +3,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Models\VerificationToken;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class VerificationController extends Controller
 {
 
-    public function index()
+    public function index(): View|RedirectResponse
     {
-
+        try{
+            return view('auth.verify');
+        } catch (\Exception | \Error $e){
+            return $this->backError('Error: ' . $e->getMessage());
+        }
     }
 
-    public function sendVerificationMail()
+    public function sendVerificationMail(): RedirectResponse
     {
+        try{
+            $user = User::where('email', request('email'))->first();
+
+            $user->mail(template: "registration-welcome-mail", replaceable: []);
+
+            return  $this->backSuccess(message: "Verification mail is sent to your email " . request('email'));
+
+        } catch (\Exception | \Error $e){
+            return $this->backError('Error: ' . $e->getMessage());
+        }
 
     }
 
@@ -25,21 +41,30 @@ class VerificationController extends Controller
             ->with('user')
             ->first();
 
-        $message = 'Sorry your email cannot be identified.';
-
         if(!is_null($verifyUser) ){
             $user = $verifyUser->user;
+            if(is_null($user->email_verified_at)) {
 
-            if(!$user->is_email_verified) {
-                $verifyUser->user->is_email_verified = 1;
-                $verifyUser->user->save();
-                $message = "Your e-mail is verified. You can now login.";
+                $user->update([
+                    "email_verified_at" => now(),
+                ]);
+
+                $message = "Your e-mail is verified. You can now login to your account.";
             } else {
-                $message = "Your e-mail is already verified. You can now login.";
+                $message = "Your e-mail is already verified. You can login to your account.";
             }
-        }
 
-        return redirect()->route('login')->with('message', $message);
+            $user->mail(template: "email-verification-success-mail", replaceable: [
+                "{login_link}" => route('auth.login'),
+            ]);
+
+            // Delete token after successful verification.
+            $verifyUser->delete();
+
+        } else {
+            return $this->redirectError(route: route('auth.login'), message: 'Sorry your email cannot be identified.');
+        }
+        return $this->redirectSuccess(route: route('auth.login'), message: $message);
     }
 
 }
