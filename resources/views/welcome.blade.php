ab6a9b0c0dbe4225cedc2391a31319bc8d2b3a35
Shoaib Khan <devsbuddy404@gmail.com>
2022-04-30 15:37:25 +0530

    Updated Landing page's content and assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,6 +58,13 @@
                 <li><a href="features.html">Features</a></li>
                 <li><a href="blog.html">Blog</a></li>
                 <li><a href="contact.html">Contact Us</a></li>
+                <li>
+                    <a href="https://github.com/thedevsbuddy/adminr" target="_blank">
+                        <svg fill="currentColor" style="width: 24px; height: 24px" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 28 28">
+                            <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"/>
+                        </svg>
+                    </a>
+                </li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
@@ -100,11 +107,13 @@
                         </p>
                     </div>
                     <div class="col-lg-5 iphone-wrap text-right">
-                        <img src="{{ asset('landing/assets/img/phone_1.png') }}" alt="Image" class="phone-1"
+                        <img src="{{ asset('landing/assets/img/hero-right-image.svg') }}" alt="Image"
+                             style="max-width: 65%"
+                             class="d-block mx-auto my-5 my-md-auto"
                              data-aos="fade-right">
-                        <img src="{{ asset('landing/assets/img/phone_2.png') }}" alt="Image" class="phone-2"
-                             data-aos="fade-right"
-                             data-aos-delay="200">
+{{--                        <img src="{{ asset('landing/assets/img/phone_2.png') }}" alt="Image" class="phone-2"--}}
+{{--                             data-aos="fade-right"--}}
+{{--                             data-aos-delay="200">--}}
                     </div>
                 </div>
             </div>
@@ -122,7 +131,7 @@
             <div class="row justify-content-center text-center mb-5">
                 <div class="col-md-5" data-aos="fade-up">
                     <h2 class="section-heading">Build projects faster with AdminR</h2>
-                    <p>You can save around 80% of your time by using our CRUD generator.</p>
+                    <p>You can save upto 80% of your time by using our CRUD generator.</p>
                 </div>
             </div>
 
@@ -130,28 +139,28 @@
                 <div class="col-md-4" data-aos="fade-up" data-aos-delay="">
                     <div class="feature-1 text-center">
                         <div class="wrap-icon icon-1">
-                            <i class="bi bi-people"></i>
+                            <i class="bi bi-hammer"></i>
                         </div>
-                        <h3 class="mb-3">Explore Your Team</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
+                        <h3 class="mb-3">Generate Resources</h3>
+                        <p>You can generate any CRUD resource with beautiful GUI.</p>
                     </div>
                 </div>
                 <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
                     <div class="feature-1 text-center">
                         <div class="wrap-icon icon-1">
-                            <i class="bi bi-brightness-high"></i>
+                            <i class="bi bi-gear-wide-connected"></i>
                         </div>
-                        <h3 class="mb-3">Digital Whiteboard</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
+                        <h3 class="mb-3">Generate APIs</h3>
+                        <p>AdminR allows you to generate APIs with the Resources.</p>
                     </div>
                 </div>
                 <div class="col-md-4" data-aos="fade-up" data-aos-delay="200">
                     <div class="feature-1 text-center">
                         <div class="wrap-icon icon-1">
-                            <i class="bi bi-bar-chart"></i>
+                            <i class="bi bi-asterisk"></i>
                         </div>
-                        <h3 class="mb-3">Design To Development</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
+                        <h3 class="mb-3">Manage Permissions</h3>
+                        <p>We allow you to manage APIs and CRUD permissions with ease.</p>
                     </div>
                 </div>
             </div>
@@ -162,57 +171,31 @@
     <section class="section">
 
         <div class="container">
-            <div class="row justify-content-center text-center mb-5" data-aos="fade">
-                <div class="col-md-6 mb-5">
-                    <img src="{{ asset('landing/assets/img/undraw_svg_1.svg')}}" alt="Image" class="img-fluid">
+            <div class="row justify-content-center text-center mb-5">
+                <div class="col-md-5" data-aos="fade-up">
+                    <h2 class="section-heading">Key Features</h2>
+                    <p>We cover some major features to help you generate or create admin panel with ease.</p>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="step">
-                        <span class="number">01</span>
-                        <h3>Sign Up</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="step">
-                        <span class="number">02</span>
-                        <h3>Create Profile</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="step">
-                        <span class="number">03</span>
-                        <h3>Enjoy the app</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, optio.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </section>
-
-    <section class="section">
-        <div class="container">
             <div class="row align-items-center">
                 <div class="col-md-4 me-auto">
-                    <h2 class="mb-4">Seamlessly Communicate</h2>
-                    <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur at reprehenderit
-                        optio,
-                        laudantium eius quod, eum maxime molestiae porro omnis. Dolores aspernatur delectus impedit
-                        incidunt
-                        dolore mollitia esse natus beatae.</p>
-                    <p><a href="#" class="btn btn-primary">Download Now</a></p>
+                    <h2 class="mb-4">CRUD Generation</h2>
+                    <p class="mb-4">
+                        AdminR has a powerful tool to generate CRUDs by using easy to use GUI
+                        which can generate a CRUD for you within minutes, And save you a lot of time which you
+                        invest while writing code by yourself.
+                    </p>
+                    <p><a href="{{ route(config('app.route_prefix'). '.builder') }}" class="btn btn-primary">Start Generating</a></p>
                 </div>
                 <div class="col-md-6" data-aos="fade-left">
                     <img src="{{ asset('landing/assets/img/undraw_svg_2.svg')}}" alt="Image" class="img-fluid">
                 </div>
             </div>
         </div>
+
     </section>
+
 
     <section class="section">
         <div class="container">
