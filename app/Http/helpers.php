db6390a55749f81abc93b010950659498272d8d6
Shoaib Khan <devsbuddy404@gmail.com>
2022-05-09 01:00:24 +0530

    Optimized getSetting query

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -7,8 +7,8 @@ use Spatie\Permission\Models\Role;
 if (!function_exists('getSetting')) {
     function getSetting($option): ?string
     {
-        return Setting::where('option', $option)->first()
-            ? Setting::where('option', $option)->first()->value
+        return Setting::where('option', $option)->count() > 0
+            ? Setting::where('option', $option)->value('value')
             : null;
     }
 }
